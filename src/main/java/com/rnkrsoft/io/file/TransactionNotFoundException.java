679058ec962d1819290bf75d08f990799feb71e5
devops4j <devops4j@devops4j.com>
2018-11-17 11:37:24 +0800

    修改

--- src/main/java/com/rnkrsoft/io/file/TransactionNotFoundException.java
+++ src/main/java/com/rnkrsoft/io/file/TransactionNotFoundException.java
@@ -3,7 +3,7 @@ package com.rnkrsoft.io.file;
 import java.io.IOException;
 
 /**
- * Created by devops4j on 2018/2/16.
+ * Created by rnkrsoft.com on 2018/2/16.
  */
 public class TransactionNotFoundException extends IOException{
     public TransactionNotFoundException(String message) {
